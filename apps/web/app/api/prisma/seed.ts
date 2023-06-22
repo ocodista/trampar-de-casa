7039fd3ead16cf7755a470b173dca44962bb938e
Caio Borghi <caiohoborghi@gmail.com>
2023-06-22 19:28:59 -0300

    Fix build

--- apps/web/app/api/prisma/seed.ts
+++ apps/web/app/api/prisma/seed.ts
@@ -4,18 +4,18 @@ const client = new PrismaClient();
 
 const getSubscribers = (): Prisma.InscritosCreateInput[] => {
 
-    return [
-        {
-           email: 'mateus.vnlima@gmail.com' 
-        },
-        {
-            email: 'caiohoborghi@gmail.com'
-        }
-    ]
+  return [
+    {
+      email: 'mateus.vnlima@gmail.com' 
+    },
+    {
+      email: 'caiohoborghi@gmail.com'
+    }
+  ]
 }
 
-void async function(){
-    const subscribers = await Promise.all(
-        getSubscribers().map(async (subscriber) => client.inscritos.create({ data: subscriber }))
-    )
+void async function (){
+  const subscribers = await Promise.all(
+    getSubscribers().map(async (subscriber) => client.inscritos.create({ data: subscriber }))
+  )
 }()
