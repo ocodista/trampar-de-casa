cdade3fc9abc149ebc391229d1a3e81711deb0fb
Caio Borghi <caiohoborghi@gmail.com>
2023-06-22 11:30:17 -0300

    Adicionar favicons

--- apps/web/app/layout.jsx
+++ apps/web/app/layout.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import "./global.css"
 
 export const metadata = {
@@ -10,6 +11,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ptbr">
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+      </Head>
       <body>{children}</body>
     </html>
   );
