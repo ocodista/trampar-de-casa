b503bff5277e64b8eabd8879190debae111520aa
Caio Borghi <caiohoborghi@gmail.com>
2023-06-22 18:02:34 -0300

    add eslint, ts, react-hook-form, zod, supabase client

---
+++ apps/web/app/api/subscribers/route.tsx
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { Entities } from "../../../global/enums/entities";
+import { createClient } from "@supabase/supabase-js";
+
+interface EmailRequest {
+  email: string;
+}
+
+export async function POST(requisicao: Request) {
+  const { email } = (await requisicao.json()) as EmailRequest;
+  if (email) {
+    return new NextResponse(null, { status: 403 });
+  }
+
+  const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY
+  );
+
+  const { data, error } = await supabase
+    .from(Entities.Subcribers)
+    .insert({ email })
+    .select();
+
+  if (error) {
+    console.error(error);
+    return new NextResponse(null, { status: 500 });
+  }
+
+  return NextResponse.json(data);
+}
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
+    "./domains/**/*.{js,ts,jsx,tsx,mdx}",
+    "./global/**/*.{js,ts,jsx,tsx,mdx}",
 
     // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
