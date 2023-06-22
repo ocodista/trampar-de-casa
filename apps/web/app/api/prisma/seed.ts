c1cdc49f65d5f55acc64d907a5440ca26b426271
anti-duhring <mateus.limavn@gmail.com>
2023-06-22 17:57:48 -0300

    setup

--- apps/web/app/api/prisma/seed.ts
+++ apps/web/app/api/prisma/seed.ts
@@ -22,11 +22,3 @@ void async function(){
         getSubscribers().map(async (subscriber) => client.inscritos.create({ data: subscriber }))
     )
 }()
-
-
-/*
-SUPABASE_URL=https://xkllxuhvzzrqokqiszok.supabase.co
-SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrbGx4dWh2enpycW9rcWlzem9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODczOTcwODUsImV4cCI6MjAwMjk3MzA4NX0.mzmdczt3BIB5iwChRXDiOXopI-ysx9RkGyLc_YDtEdI
-Caio Borghi17:49
-hbw9FXH0bcx_anv7cwz
-*/
