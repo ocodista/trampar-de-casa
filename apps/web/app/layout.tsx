acef25fa4361956d1c9dafd865eededf81e601d1
LukeberryPi <lukeberrypi@gmail.com>
2023-06-23 00:08:51 -0300

    feat: add smooth scrolling

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout ({ children }) {
   return (
-    <html lang="ptbr">
+    <html className="scroll-smooth" lang="ptbr">
       <body>
         {children}
         <Toaster />
