617b569b08cd4bb1f026e6d6c89f38499792cb17
Alberto Pluecker <52082366+albertofp@users.noreply.github.com>
2023-06-30 00:00:48 +0200

    Vitest unit test support (#41)

---
+++ packages/shared/tests/sample.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+describe("Sample test in /shared", () => {
+  it("true===true", () => {
+    expect(true).toBe(true);
+  });
+});
