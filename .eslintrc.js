b373bb657f0ab9fdd226a521e04ac21497d22151
Caio Borghi <caiohoborghi@gmail.com>
2023-07-03 17:19:06 -0300

    Add prettier plugin to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "next/core-web-vitals",
+    "prettier"
   ],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
