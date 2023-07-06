ab86347da7aee4cc2515515ae62db3ef470a4a05
Gabriel Souza <gabrielsouzadevweb@gmail.com>
2023-07-05 21:34:04 -0300

    refactor: move a navbar in component/ui and modify your style

---
+++ apps/web/app/components/ui/Navbar.tsx
@@ -0,0 +1,95 @@
+import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/20/solid/index'
+import Image from 'next/image'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Navbar({ navigation }) {
+  return (
+    <Disclosure as="nav" className="bg-indigo-600 ">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <a className="inline-block" href="#">
+                    <Image
+                      className="block h-8 w-auto lg:hidden"
+                      width={70}
+                      height={70}
+                      src="images/logo.svg"
+                      alt="Logotipo da Trampar De Casa"
+                    />
+                  </a>
+                  <a className="inline-block" href="#">
+                    <Image
+                      className="hidden h-8 w-auto lg:block"
+                      width={70}
+                      height={70}
+                      src="images/logo.svg"
+                      alt="Logotipo da Trampar De Casa"
+                    />
+                  </a>
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? 'bg-indigo-800 text-white '
+                            : 'text-gray-300 hover:bg-indigo-700 hover:text-white  ',
+                          'rounded-md px-3 py-2 text-sm font-medium '
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? 'bg-indigo-800 text-white'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
