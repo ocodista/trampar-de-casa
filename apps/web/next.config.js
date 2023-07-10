6282d70e789fe22fd467be67362137d324e2c145
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2023-07-10 15:07:21 -0300

    fix: workaround for prisma

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
+
 module.exports = {
-  reactStrictMode: true,
-  transpilePackages: ["ui", "eslint-config-custom", "shared"],
   experimental: {
     serverActions: true
+  },
+  reactStrictMode: true,
+  transpilePackages: ["ui", "eslint-config-custom", "shared"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
   }
-};
+}
