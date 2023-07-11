c49ec9dfc65feb02b210de5b445ff82c1110323f
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2023-07-11 16:11:48 -0300

    chore: remove prisma workaround

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,17 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
-
 module.exports = {
   experimental: {
     serverActions: true
   },
   reactStrictMode: true,
   transpilePackages: ["ui", "eslint-config-custom", "shared"],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()]
-    }
-
-    return config
-  }
 }
