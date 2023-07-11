3b176e493915ed457c59ae2745c89f43ebc361a1
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2023-07-11 16:16:59 -0300

    chore: add prisma plugin and db package on transpilePackage

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
+
 module.exports = {
   experimental: {
     serverActions: true
   },
   reactStrictMode: true,
-  transpilePackages: ["ui", "eslint-config-custom", "shared"],
+  transpilePackages: ["ui", "eslint-config-custom", "shared", "db"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  }
 }
