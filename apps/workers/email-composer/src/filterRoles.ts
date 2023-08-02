1c4eddf8aa4b80f30a768862c4b704f480c4102e
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2023-08-02 17:32:40 -0300

    refactor: improve structure of filterRoles

--- apps/workers/email-composer/src/filterRoles.ts
+++ apps/workers/email-composer/src/filterRoles.ts
@@ -1,33 +1,20 @@
-import { getSupabaseClient } from 'db'
 import { createClient } from 'redis'
-import { Entities } from 'shared'
 import { RedisPrefix } from 'shared/src/enums/redis'
 
 export const filterRoles = async (rolesId: string[]) => {
-  const filteredRoles: Record<string, string> = {}
-  const supabase = getSupabaseClient()
+  let concatenatedHtmlRoles = ''
   const redisClient = createClient()
   await redisClient.connect()
 
-  const getReadyRole = async (roleId: string) => {
-    const { data } = await supabase
-      .from(Entities.Roles)
-      .select('*')
-      .eq('id', roleId)
-      .eq('ready', 'TRUE')
+  for (let index = 0; index < rolesId.length; index++) {
+    const roleId = rolesId[index]
 
-    return data
+    const roleHtml = await redisClient.get(
+      `${RedisPrefix.RolesRenderer}${roleId}`
+    )
+    if (!roleHtml) break
+    concatenatedHtmlRoles = `${concatenatedHtmlRoles}${roleHtml}`
   }
-
-  rolesId.forEach(async (roleId) => {
-    const data = await getReadyRole(roleId)
-    if (!data?.length) return
-
-    const roleHtml = await redisClient.get(`${RedisPrefix}${roleId}`)
-    if (!roleHtml) return
-    filteredRoles[roleId] = roleHtml
-  })
-
   await redisClient.disconnect()
-  return filteredRoles
+  return concatenatedHtmlRoles
 }
--- apps/workers/email-composer/src/test/filterRoles.spec.ts
+++ apps/workers/email-composer/src/test/filterRoles.spec.ts
@@ -1,21 +1,17 @@
 import { faker } from '@faker-js/faker'
-import * as getSupabaseClientFile from 'db'
 import * as redisFile from 'redis'
 import {
-  getSupabaseClientStub,
   redisConnectStub,
   redisDisconnectStub,
+  redisGetStub,
   redisStub,
 } from 'shared/src/test/helpers/stubs'
 import { filterRoles } from 'src/filterRoles'
-import { vi } from 'vitest'
+import { expect, vi } from 'vitest'
 
-describe.only('filter roles', () => {
+describe('filter roles', () => {
   beforeEach(() => {
     vi.spyOn(redisFile, 'createClient').mockImplementation(redisStub)
-    vi.spyOn(getSupabaseClientFile, 'getSupabaseClient').mockImplementation(
-      getSupabaseClientStub
-    )
   })
   it('establish connection with redis', async () => {
     await filterRoles([faker.string.sample()])
@@ -26,5 +22,16 @@ describe.only('filter roles', () => {
     await filterRoles([faker.string.sample()])
 
     expect(redisDisconnectStub).toBeCalled()
+  })
+  it('concatenate roles taken on redis', async () => {
+    const rolesIdArray = [faker.string.uuid(), faker.string.uuid()]
+    const firstRoleHTML = faker.string.sample()
+    const twoRoleHTML = faker.string.sample()
+    redisGetStub.mockResolvedValueOnce(firstRoleHTML)
+    redisGetStub.mockResolvedValueOnce(twoRoleHTML)
+
+    const concatenatedRoles = await filterRoles(rolesIdArray)
+
+    expect(concatenatedRoles).toStrictEqual(`${firstRoleHTML}${twoRoleHTML}`)
   })
 })
