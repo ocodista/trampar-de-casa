0565b1580066a5bc451ee4155b91e3e25bcb8d63
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2023-08-04 11:11:34 -0300

    refactor: return process.exit when emailSender finish

--- apps/workers/email-sender/src/index.ts
+++ apps/workers/email-sender/src/index.ts
@@ -1,3 +1,5 @@
 import { emailSender } from './emailSender'
 
-emailSender().catch((e) => console.log('email sender service error:', e))
+emailSender()
+  .catch((e) => console.log('email sender service error:', e))
+  .finally(() => process.exit(0))
