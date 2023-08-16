a3d5d1f9369b06db479ab8b0802fa4446b624506
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2023-08-16 17:26:57 -0300

    refactor: change puppeteer config

--- apps/workers/roles-validator/src/launchBrowserAndNavigateTo.ts
+++ apps/workers/roles-validator/src/launchBrowserAndNavigateTo.ts
@@ -2,9 +2,8 @@ import puppeteer from 'puppeteer'
 
 export const launchBrowserAndNavigateToPage = async (url: string) => {
   const browser = await puppeteer.launch({
-    // eslint-disable-next-line turbo/no-undeclared-env-vars
-    executablePath: process.env.CHROME_BIN || undefined,
     args: ['--no-sandbox', '--headless', '--disable-gpu'],
+    headless: false,
   })
   const page = await browser.newPage()
 
