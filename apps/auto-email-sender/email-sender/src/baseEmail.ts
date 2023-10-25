f6b2121b7f8ca3cd528bd26b930d15f61b46ca49
Joao Victor <santosjoao.dev@gmail.com>
2023-10-24 21:18:14 -0300

    refactor: disable roles count on subject

--- apps/auto-email-sender/email-sender/src/baseEmail.ts
+++ apps/auto-email-sender/email-sender/src/baseEmail.ts
@@ -1,3 +1,2 @@
 export const EMAIL_FROM = 'Trampar de Casa <comece@trampardecasa.com.br>'
-export const rolesSubject = (roles: number) =>
-  `${roles} Vagas para você Trampar de Casa`
+export const rolesSubject = (roles: number) => `Vagas para você Trampar de Casa`
