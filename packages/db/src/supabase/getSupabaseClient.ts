73e9e814a7d6f1c69338b06c241ebaf2dac4275a
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2023-10-26 14:06:44 -0300

    fix: type return

--- packages/db/src/supabase/getSupabaseClient.ts
+++ packages/db/src/supabase/getSupabaseClient.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import { Database } from './type'
 
 dotenv.config()
-export const getSupabaseClient = (): SupabaseClient => {
+export const getSupabaseClient = (): SupabaseClient<Database> => {
   const client = createClient<Database>(
     process.env['SUPABASE_URL'] || '',
     process.env['SUPABASE_SERVICE_ROLE'] || ''
