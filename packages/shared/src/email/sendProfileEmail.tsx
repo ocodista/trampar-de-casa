10e6b32a6ab03ba3aea520d7cec0c2946eef0555
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2023-11-10 16:18:17 -0300

    refactor: finish send profile email function

--- packages/shared/src/email/sendProfileEmail.tsx
+++ packages/shared/src/email/sendProfileEmail.tsx
@@ -1,5 +1,7 @@
 import { render } from '@react-email/render'
 import { SupabaseTable } from 'db/src/supabase/utilityTypes'
+import fs from 'fs'
+import path from 'path'
 import React from 'react'
 import { Resend } from 'resend'
 import ProfileEmailTemplate from '../../emails/ProfileEmail'
@@ -8,25 +10,30 @@ import { encrypt } from '../security'
 type Subscriber = SupabaseTable<'Subscribers'>
 const TEMPLATE_EMAIL = render(<ProfileEmailTemplate />)
 const EMAIL_SUBJECT = '⚙️ Configure suas preferências de vagas!' as const
+const resend = new Resend(process.env['RESEND_KEY'])
 
 const profileUrl = (encryptedId: string) =>
-  `https://trampardecasa.com.br/subscribers/profile/${encryptedId}`
+  `https://trampar-de-casa-7lkvipswn-trampar-de-casa.vercel.app/subscribers/profile/${encryptedId}`
 export const sendProfileEmail = async ({
   email,
   id,
 }: Pick<Subscriber, 'email' | 'id'>) => {
-  console.time(`sendProfileEmail#${email}`)
   const cryptSecret = process.env['CRYPT_SECRET']
   if (!cryptSecret) throw new Error('secret not found')
 
-  const resend = new Resend(process.env['RESEND_KEY'])
   const encryptedId = encrypt(cryptSecret, id)
 
-  await resend.emails.send({
-    from: 'Trampar de Casa <comece@trampardecasa.com.br>',
-    to: email,
-    html: TEMPLATE_EMAIL.replaceAll('$PROFILE_URL', profileUrl(encryptedId)),
-    subject: EMAIL_SUBJECT,
-  })
-  console.timeEnd(`sendProfileEmail#${email}`)
+  try {
+    await resend.emails.send({
+      from: 'Trampar de Casa <comece@trampardecasa.com.br>',
+      to: email,
+      html: TEMPLATE_EMAIL.replaceAll('$PROFILE_URL', profileUrl(encryptedId)),
+      subject: EMAIL_SUBJECT,
+    })
+  } catch {
+    fs.appendFileSync(
+      path.resolve(__dirname, `./failed-preference-emails.txt`),
+      `${id},${email}\n`
+    )
+  }
 }
