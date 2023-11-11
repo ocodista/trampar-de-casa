2161b9b525ed10a05fda3ff8bfb8141ac6bc021c
Caio Borghi <caiohoborghi@gmail.com>
2023-11-10 21:19:54 -0300

    Fix best openings (#177)

--- apps/web/app/api/subscribers/[id]/route.ts
+++ apps/web/app/api/subscribers/[id]/route.ts
@@ -31,12 +31,15 @@ export async function PUT(request: Request) {
         distinct_id: data[0].email,
       }
     )
-    new Tracker(process.env['NEXT_PUBLIC_MIXPANEL_KEY']).track(
-      Events.ReceiveBestOpenings,
-      {
-        distinct_id: data[0].email,
-      }
-    )
+
+    if (data[0].sendBestOpenings) {
+      new Tracker(process.env['NEXT_PUBLIC_MIXPANEL_KEY']).track(
+        Events.ReceiveBestOpenings,
+        {
+          distinct_id: data[0].email,
+        }
+      )
+    }
     if (skillsSuggestions.length) {
       const supabase = getSupabaseClient()
       const suggestionsInsertPromise = skillsSuggestions.map((skill) => {
