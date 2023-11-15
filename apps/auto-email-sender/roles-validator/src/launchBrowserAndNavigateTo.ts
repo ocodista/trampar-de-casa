97ffde65fd68c918ce748fa92f9c0d9abf18b1ea
Caio Borghi <caiohoborghi@gmail.com>
2023-11-15 17:22:54 -0300

    remove sleep from roles validator

--- apps/auto-email-sender/roles-validator/src/launchBrowserAndNavigateTo.ts
+++ apps/auto-email-sender/roles-validator/src/launchBrowserAndNavigateTo.ts
@@ -1,6 +1,5 @@
 import puppeteer from 'puppeteer'
 
-const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
 export const launchBrowserAndNavigateToPage = async (url: string) => {
   console.time(`launchBrowserAndNavigateToPage#${url}`)
   const browser = await puppeteer.launch({
@@ -10,8 +9,6 @@ export const launchBrowserAndNavigateToPage = async (url: string) => {
   const page = await browser.newPage()
 
   await page.goto(url)
-  await sleep(6_000)
-
   console.timeEnd(`launchBrowserAndNavigateToPage#${url}`)
   return { page, browser }
 }
--- apps/auto-email-sender/roles-validator/src/searchRoleOnPageContent.ts
+++ apps/auto-email-sender/roles-validator/src/searchRoleOnPageContent.ts
@@ -1,8 +1,6 @@
 import { Page } from 'puppeteer'
 
 export const searchRoleOnPageContent = async (page: Page, role: string) => {
-  console.time(`searchRoleOnPageContent#${role}`)
   const html = await page.content()
-  console.timeEnd(`searchRoleOnPageContent#${role}`)
   return Boolean(html.includes(role))
 }
