a41e86b1102341f537cb122e09539e23aa4c2414
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2023-11-29 17:38:38 -0300

    test: add more logs

--- apps/auto-email-sender/email-composer/src/getHtmlRoles.ts
+++ apps/auto-email-sender/email-composer/src/getHtmlRoles.ts
@@ -15,18 +15,22 @@ export const getHtmlRoles = async (
 ): Promise<string> => {
   const roles: RolesRendererCollection[] = []
   let role
+  console.time('getHtmlRoles [For Loop]')
   for (const roleId of rolesId) {
     role = memoizedRoles.get(roleId)
     if (!role) {
+      console.time('getHtmlRoles [mongoCollection.findOne]')
       role = await mongoCollection.findOne<RolesRendererCollection>({
         id: roleId,
       })
       memoizedRoles.set(roleId, role)
+      console.timeEnd('getHtmlRoles [mongoCollection.findOne]')
     }
     if (role) {
       roles.push(role)
     }
   }
+  console.timeEnd('getHtmlRoles [For Loop]')
 
   return RenderRolesSection({
     roles,
