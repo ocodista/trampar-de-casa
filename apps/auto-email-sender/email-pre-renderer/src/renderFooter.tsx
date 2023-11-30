d5b1be943a694efbd4d6455acfb2632cf3705d12
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2023-11-30 15:20:55 -0300

    refactor: improve performance

--- apps/auto-email-sender/email-pre-renderer/src/renderFooter.tsx
+++ apps/auto-email-sender/email-pre-renderer/src/renderFooter.tsx
@@ -1,10 +1,37 @@
+import { Link, Tailwind, Text } from '@react-email/components'
 import { render } from '@react-email/render'
 import React from 'react'
-import { Footer } from './Footer'
 import { getUnsubscribeLink } from './getUnsubscribeLink'
 
-export function renderFooter(id: string, urlPrefix: string) {
+enum HtmlKey {
+  unsubscribeLink = '##UNSUBSCRIBE_LINK',
+}
+
+export const renderFooterHTML = () => {
+  const renderedHtml = render(
+    <Tailwind>
+      <Text className="text-left text-[8px] leading-[24px] text-[#525f7f]">
+        Se por algum motivo você deseja interromper o recebimento destas
+        comunicações, entendemos completamente. Nós respeitamos o seu espaço e o
+        valorizamos como membro da nossa comunidade. Para se desinscrever, basta
+        clicar{' '}
+        <Link className="text-[#556cd6]" href={HtmlKey.unsubscribeLink}>
+          aqui
+        </Link>
+        .
+      </Text>
+    </Tailwind>
+  )
+
+  return renderedHtml
+}
+
+export function mountFooter(
+  id: string,
+  urlPrefix: string,
+  renderedFooter: string
+) {
   const unsubscribeLink = getUnsubscribeLink(urlPrefix, id)
 
-  return render(<Footer unsubscribeUrl={unsubscribeLink} />)
+  return renderedFooter.replace(HtmlKey.unsubscribeLink, unsubscribeLink)
 }
