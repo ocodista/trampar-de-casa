bbc0b6cfc7e1817068bb9d1a922a7922ab035e52
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2023-12-07 14:29:30 -0300

    feat: add description element and toast

--- apps/web/app/subscribers/testemunho/[id]/page.tsx
+++ apps/web/app/subscribers/testemunho/[id]/page.tsx
@@ -2,6 +2,7 @@
 import { getDecryptedId } from 'app/api/getDecryptedId'
 import { getSupabaseClient } from 'db'
 import { TestimonialForm } from './TestimonialForm'
+import { ToastContainer } from './ToastContainer'
 
 type PageProps = { params: { id: string } }
 
@@ -18,13 +19,27 @@ const getSubscriberEmail = async (userId: string) => {
   return data[0].email
 }
 
+const Typography = {
+  h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
+  p: 'leading-7 [&:not(:first-child)]:mt-6',
+}
+
 export default async function TestimonialsPage({ params }: PageProps) {
   const subscriberId = getDecryptedId(params.id)
   const email = await getSubscriberEmail(subscriberId)
 
   return (
-    <section className="grid flex-1 place-content-center">
+    <section className="grid flex-1 place-content-center space-y-5">
+      <section className="mx-4">
+        <h1 className={Typography.h1}>Fui contratado</h1>
+        <p className={Typography.p}>
+          Ficamos felizes em te ajudar a encontrar a vaga ideal!
+          <br />
+          Que tal nos explicar mais sobre como foi?
+        </p>
+      </section>
       <TestimonialForm email={email} />
+      <ToastContainer />
     </section>
   )
 }
