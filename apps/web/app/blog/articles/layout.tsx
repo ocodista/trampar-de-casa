a5fecf2af4a3111ce99debce7a46023d54dc250c
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2024-02-16 14:13:54 -0300

    fix: css module importation

--- apps/web/app/blog/articles/layout.tsx
+++ apps/web/app/blog/articles/layout.tsx
@@ -1,11 +1,11 @@
 import { ReactNode } from 'react'
-import { article } from './article.module.css'
+import style from './article.module.css'
 
 export default function Layout({ children }: { children: ReactNode }) {
   const styleClass =
     'relative w-full #bg-white px-6 py-12 #shadow-xl #shadow-slate-700/10 #ring-1 ring-gray-900/5 md:mx-auto md:max-w-3xl lg:max-w-4xl lg:pb-28 lg:pt-16 prose mx-auto max-w-prose lg:text-lg'
   return (
-    <article className={`container ${article} ${styleClass}`}>
+    <article className={`container ${style.article} ${styleClass}`}>
       {children}
     </article>
   )
