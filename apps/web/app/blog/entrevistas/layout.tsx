00fe9108868ac2dd617eaad26ef79e0ea26f3bf9
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2024-02-20 16:18:06 -0300

    fix: move route segments to a valid file

--- apps/web/app/blog/entrevistas/layout.tsx
+++ apps/web/app/blog/entrevistas/layout.tsx
@@ -3,6 +3,11 @@ import style from './article.module.css'
 import ScrollToTopButton from 'app/components/ui/ScrollToTopButton'
 import { ArticleReader } from './ArticleReader'
 
+export const revalidate = 1 // Needed to increase counter
+
+// Opt out of caching for all data requests in the route segment
+export const dynamic = 'force-dynamic'
+
 export default function Layout({ children }: { children: ReactNode }) {
   const styleClass =
     'relative w-full #bg-white px-6 py-12 #shadow-xl #shadow-slate-700/10 #ring-1 ring-gray-900/5 md:mx-auto md:max-w-3xl lg:max-w-4xl lg:pb-28 lg:pt-16 prose mx-auto max-w-prose lg:text-lg pt-0'
