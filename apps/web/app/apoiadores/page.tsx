76063e5127057d96ee2504ce6d83c17cbf464d10
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-03-11 19:12:38 -0300

    feat: SupportingCompanies screen

---
+++ apps/web/app/apoiadores/page.tsx
@@ -0,0 +1,66 @@
+import { FocusBanner } from 'app/landing-page/FocusBanner'
+import { Github } from 'lucide-react'
+import hearth from '../../public/images/hearth.png'
+import CompanyCard from 'app/components/ui/CompanyCard'
+import { companies } from 'app/landing-page/PartnerCompanies'
+import { useEffect, useState } from 'react'
+
+const SupportingCompanies = () => {
+  return (
+    <>
+      <FocusBanner />
+      <div className="container mx-auto sm:pt-16 md:pt-20 lg:pt-28">
+        <div className="flex h-[300px] pt-[30px] sm:h-[350px] md:h-[400px] lg:h-[479px]">
+          <div className="flex w-3/6 flex-col items-center justify-center">
+            <p className="text-[15px]  font-bold sm:text-[30px] md:text-[45px] lg:text-[60px]">
+              Empresas que apoiam o trabalho remoto
+            </p>
+            <p className="pt-[20px] text-[12px] sm:pt-[25px] sm:text-[18px] md:pt-[30px] md:text-[20px] lg:pt-[35px] lg:text-[26px]">
+              O trabalho remoto tem se tornado cada vez mais comum, e várias
+              empresas ao redor do mundo têm adotado essa prática para
+              proporcionar flexibilidade e uma vida melhor para seus
+              funcionários.
+            </p>
+          </div>
+          <div className="flex w-3/6 items-center justify-end">
+            <img
+              className=" h-[160px] w-[160px] scale-x-[-1] transform sm:h-[250px] sm:w-[250px] md:h-[300px] md:w-[300px] lg:h-[368px] lg:w-[385px]"
+              src={hearth.src}
+              alt="Hearth Image"
+            />
+          </div>
+        </div>
+        <div className="mt-14 sm:mt-20 md:mt-32 lg:mt-36">
+          <p className="mb-[50px] text-[20px] font-bold sm:text-[27px] md:text-[27px] lg:text-[27px]">
+            Empresas que apoiam:
+          </p>
+          <div className="flex flex-wrap justify-center gap-5 pb-16 sm:gap-5 md:gap-10 lg:gap-16">
+            {companies.map((company) => (
+              <CompanyCard
+                key={company.name}
+                companyWebsite={company.url}
+                logoSrc={company.imagePath}
+                companyName={company.name}
+                description={company.description}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="flex h-[50px] items-center justify-between sm:h-[60px] md:h-[80px] lg:h-[100px]">
+          <p className="text-[14px] font-medium sm:text-[16px] md:text-[18px] lg:text-[20px]">
+            ©2024 Trampar de Casa
+          </p>
+          <a
+            href={'https://github.com/ocodista/trampar-de-casa'}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Github className="h-[26px] w-[26px] sm:h-[28px] sm:w-[28px] md:h-[30px] md:w-[30px] lg:h-[32px] lg:w-[32px]" />
+          </a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SupportingCompanies
