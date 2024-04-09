1418825af1001015f8ed6307d498ee946223692a
Caio Borghi <caiohoborghi@gmail.com>
2024-04-08 21:22:39 -0300

    add posthog

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -3,12 +3,22 @@
 import React, { FC, ReactNode } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { LoadingProvider } from './contexts/LoadingContext'
+import { PostHogProvider } from 'posthog-js/react'
+import posthog from 'posthog-js'
+
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  })
+}
 
 export const Providers: FC<{ children: ReactNode }> = ({ children }) => {
   const queryClient = new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
-      <LoadingProvider>{children}</LoadingProvider>
+      <PostHogProvider client={posthog}>
+        <LoadingProvider>{children}</LoadingProvider>
+      </PostHogProvider>
     </QueryClientProvider>
   )
 }
