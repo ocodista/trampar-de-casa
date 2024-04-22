f6a6c36ba8bb36ab2b49f8dc4f6f0c358c3085bb
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2024-04-22 16:04:31 -0300

    feat: enable posthog debug mode on development env

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,14 +1,19 @@
 'use client'
 
-import React, { FC, ReactNode } from 'react'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+import { FC, ReactNode } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { LoadingProvider } from './contexts/LoadingContext'
-import { PostHogProvider } from 'posthog-js/react'
-import posthog from 'posthog-js'
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      // eslint-disable-next-line turbo/no-undeclared-env-vars
+      if (process.env.NODE_ENV === 'development') posthog.debug()
+    },
   })
 }
 
