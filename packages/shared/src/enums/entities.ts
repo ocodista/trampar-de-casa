64d8e78333adf9c57df07f9c8b386219aab7e3e8
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2024-04-30 09:01:32 -0300

    refactor: use type safety for table names

--- packages/shared/src/enums/entities.ts
+++ packages/shared/src/enums/entities.ts
@@ -1,8 +1,14 @@
-export enum Entities {
-  Topics = 'Topics',
-  Roles = 'Roles',
-  UserRoles = 'UserRoles',
-  Skills = 'Skills',
-  Subcribers = 'Subscribers',
-  SubscriberTopics = 'SubscriberTopics',
+import { Database } from 'db'
+
+type DbTableNames = {
+  [K in keyof Partial<Database['public']['Tables']>]?: K
+}
+
+export const Entities: DbTableNames = {
+  Topics: 'Topics',
+  Roles: 'Roles',
+  UserRoles: 'UserRoles',
+  Skills: 'Skills',
+  Subscribers: 'Subscribers',
+  SubscriberTopics: 'SubscriberTopics',
 }
