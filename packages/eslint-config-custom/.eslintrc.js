82ce690630b247f39d8abc7121dbeab207edf76f
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-06-10 16:26:28 -0300

    fix eslint error on build

---
+++ packages/eslint-config-custom/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    "next",
+    "turbo",
+    "prettier",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "prettier"
+  ],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "@next/next/no-img-element": "off",
+    "react/react-in-jsx-scope": 0,
+    "no-console": "warn",
+    "react-hooks/exhaustive-deps": "off"
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
+    },
+  },
+};
