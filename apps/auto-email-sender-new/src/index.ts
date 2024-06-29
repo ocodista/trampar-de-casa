e4647aea2f2aad459718ddab8e1ff291bed6b11f
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-06-28 21:35:51 -0300

    feat: verify build and push yml

--- apps/auto-email-sender-new/src/index.ts
+++ apps/auto-email-sender-new/src/index.ts
@@ -10,27 +10,46 @@ dotenv.config()
 
 export const init = async () => {
   try {
+    console.log('Starting subsToQueue...')
     await subsToQueue()
+    console.log('subsToQueue completed.')
 
+    console.log('Starting rolesRenderer...')
     await rolesRenderer()
+    console.log('rolesRenderer completed.')
 
+    console.log('Starting assignRoles...')
     const assignRolesPromises = Array(4)
       .fill(assignRoles)
-      .map((fn) => fn())
+      .map((fn) => {
+        console.log('Starting assignRoles...')
+        return fn()
+      })
     await Promise.all(assignRolesPromises)
+    console.log('assignRoles completed.')
 
+    console.log('Starting emailPreRender...')
     const emailPreRenderPromises = Array(4)
       .fill(emailPreRender)
-      .map((fn) => fn())
+      .map((fn) => {
+        console.log('Starting emailPreRender...')
+        return fn()
+      })
     await Promise.all(emailPreRenderPromises)
+    console.log('emailPreRender completed.')
 
+    console.log('Starting composeEmail...')
     await composeEmail()
+    console.log('composeEmail completed.')
 
+    console.log('Starting emailSender...')
     await emailSender()
+    console.log('emailSender completed.')
 
+    console.log('Process completed successfully.')
     process.exit(0)
   } catch (error) {
-    console.log(error)
+    console.error('Error during execution:', error)
     process.exit(1)
   }
 }
