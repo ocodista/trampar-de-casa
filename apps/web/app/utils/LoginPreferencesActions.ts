3d8eba48d1f131229a719424ee8c8a6a420dba46
Luís Fernando de Paulo Sousa Oliveira <luis.oliveirabr1@gmail.com>
2024-07-19 11:53:21 -0300

    feat: set settings on header (#281)

---
+++ apps/web/app/utils/LoginPreferencesActions.ts
@@ -0,0 +1,48 @@
+'use server'
+import { createClient } from '@supabase/supabase-js'
+import { encrypt } from 'shared'
+import { sendProfileEmail } from 'shared/src/email/sendProfileEmail'
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE
+)
+
+export async function encryptId(id: string) {
+  const secretKey = process.env['CRYPT_SECRET'] || ''
+
+  return encrypt(secretKey, id)
+}
+
+export async function sendEditPreferencesEmail(email: string, id: string) {
+  try {
+    await sendProfileEmail({ email, id })
+  } catch (error) {
+    console.error('Erro ao enviar e-mail de preferências:', error)
+    throw error
+  }
+}
+
+export default async function login(email: string) {
+  try {
+    const { data, error } = await supabase
+      .from('Subscribers')
+      .select('*')
+      .eq('email', email)
+      .single()
+
+    if (error && error.code === 'PGRST116') {
+      console.error('Usuário não encontrado com o e-mail:', email)
+      throw new Error('Usuário não encontrado')
+    } else if (error) {
+      console.error('Erro ao buscar usuário:', error)
+      throw error
+    }
+
+    console.log('Usuário encontrado com sucesso:')
+    return data.id
+  } catch (error) {
+    console.error('Erro ao buscar usuário:', error)
+    throw error
+  }
+}
