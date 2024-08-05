c3d466822bbd2019a6398592f88c233c11914e80
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-08-05 12:26:23 -0300

    fix: typo

--- apps/auto-email-sender-new/src/match_roles/src/train/onehot.py
+++ apps/auto-email-sender-new/src/match_roles/src/train/onehot.py
@@ -5,7 +5,7 @@ import pandas as pd
 from feature_engine import encoding
 
 # %%
-# preparação do onehot endonding
+# preparação do onehot enconding
 
 def train_onehot(entity):
     df = pd.read_csv(f'../../data/{entity}.csv')
