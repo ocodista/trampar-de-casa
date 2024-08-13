4217eecf0fe800de2d71754bf2bd23fa2ea17223
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-08-12 21:51:24 -0300

    feat: button for filter by preferences and adapted layout to work with it

--- apps/web/app/(roles)/vagas/RolesPage.tsx
+++ apps/web/app/(roles)/vagas/RolesPage.tsx
@@ -261,9 +261,9 @@ export const RolesPage = ({ jobsFromServer, skillsFromServer }) => {
           <div className="relative">{memoInputWithUseTyped}</div>
         </div>
         <div className="mb-[150px]">
-          <div className="w-full">
-            <div className="flex flex-col justify-between md:flex-row lg:flex-row">
-              <div className="xs:justify-start flex flex-wrap justify-center gap-[10px] sm:justify-between md:justify-start md:gap-[20px] lg:justify-start lg:gap-[20px]">
+          <div className="flex flex-col gap-3">
+            <div className="flex flex-col justify-between gap-2 md:flex-row lg:flex-row">
+              <div className="flex w-full flex-wrap justify-start gap-2 md:gap-5 lg:gap-5">
                 <SelectInput
                   placeholder="🔎  Área"
                   options={technologies}
@@ -298,25 +298,25 @@ export const RolesPage = ({ jobsFromServer, skillsFromServer }) => {
                   setHasMore={setHasMore}
                 />
               </div>
-              <div className="relative flex items-center">
-                <input
-                  readOnly
-                  placeholder={
-                    orderOptions.find((or) => or.value === orderButtonValue)
-                      .label
-                  }
-                  className="text-baseline border-box xs:w-[220px] z-[2] mt-[20px] w-full rounded-[20px] border-[1px] bg-transparent py-[9px] pl-[14px] 
-                  pr-[9px] placeholder-black placeholder-opacity-100 sm:w-[220px] md:mt-0 md:w-[220px] lg:mt-0 lg:w-[220px]"
-                  onFocus={() => setShowOrder(true)}
-                  onBlur={() => {
-                    setTimeout(() => setShowOrder(false), 100)
-                  }}
-                />
-                <ChevronDown className="z-1 xs:left-[185px] absolute right-[10px] mt-[20px] sm:left-[185px] md:left-[185px] md:mt-0 lg:left-[185px] lg:mt-0 " />
+              <div className="xs:justify-start relative flex items-center justify-center sm:justify-start md:justify-start lg:justify-start">
+                <div className="xs:w-auto relative flex w-auto items-center justify-end sm:w-auto md:w-auto lg:w-auto">
+                  <input
+                    readOnly
+                    placeholder={
+                      orderOptions.find((or) => or.value === orderButtonValue)
+                        .label
+                    }
+                    className="text-baseline border-box xs:w-[220px] z-[2] w-full rounded-2xl border-[1px] bg-transparent py-[9px] pl-[14px] 
+                  pr-[9px] placeholder-black placeholder-opacity-100 sm:w-[220px] md:w-[220px] lg:w-[220px]"
+                    onFocus={() => setShowOrder(true)}
+                    onBlur={() => {
+                      setTimeout(() => setShowOrder(false), 100)
+                    }}
+                  />
+                  <ChevronDown className="z-1 absolute mr-3" />
+                </div>
                 {showOrder && (
-                  <div
-                    className={`absolute left-0 top-[65px] z-10 max-h-[500px] w-[250px] overflow-y-auto rounded-[12px] bg-[#f4f4f5] p-[7px] md:right-0 md:top-[47px] lg:right-0 lg:top-[47px]`}
-                  >
+                  <div className="xs:w-[250px] absolute top-[47px] z-10 max-h-[500px] w-full overflow-y-auto rounded-2xl bg-[#f4f4f5] p-[7px] sm:w-[250px] md:w-[250px] lg:w-[250px]">
                     {orderOptions
                       .filter((or) => or.value !== null)
                       .map((option) => (
@@ -332,12 +332,20 @@ export const RolesPage = ({ jobsFromServer, skillsFromServer }) => {
                 )}
               </div>
             </div>
+            <div className="flex items-center justify-start">
+              <button
+                onClick={() => console.log('teste')}
+                className="border-box xs:w-[150px] w-full rounded-2xl border bg-transparent py-[9px] pl-2 pr-2 placeholder-black placeholder-opacity-100 sm:w-[150px] md:w-[150px] lg:w-[150px]"
+              >
+                ⭐ Preferências
+              </button>
+            </div>
             <div className="flex gap-[15px]">
               {filters.map((filter) => (
                 <div
                   key={filter.option.value}
                   className={`border-box relative mt-[15px] flex items-center 
-                  rounded-[20px] border-[1px] bg-[#F4F4F5] py-[7px] pl-[7px] 
+                  rounded-2xl border-[1px] bg-[#F4F4F5] py-[7px] pl-[7px] 
                   pr-[10px] text-center placeholder-black placeholder-opacity-100 ${
                     order.includes(filter.option.label) && 'hidden'
                   }`}
@@ -359,7 +367,7 @@ export const RolesPage = ({ jobsFromServer, skillsFromServer }) => {
                 ) && (
                   <button
                     onClick={deleteAllfilters}
-                    className="border-box relative mt-[15px] flex items-center rounded-[20px] border-[1px] border-[#FF0000] bg-[#F4F4F5] py-[7px] pl-[7px] pr-[7px] text-center text-[#FF0000] hover:bg-[#FF0000] hover:text-white"
+                    className="border-box relative mt-[15px] flex items-center rounded-2xl border-[1px] border-[#FF0000] bg-[#F4F4F5] py-[7px] pl-[7px] pr-[7px] text-center text-[#FF0000] hover:bg-[#FF0000] hover:text-white"
                   >
                     <X />
                     <p>Limpar {totalJobs} resultados</p>
