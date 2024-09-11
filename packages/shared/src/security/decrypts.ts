961122510db8113719ecbcf941d823d02383a5c5
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-09-11 13:00:09 -0300

    new test build

--- packages/shared/src/security/decrypts.ts
+++ packages/shared/src/security/decrypts.ts
@@ -2,15 +2,14 @@ import { createDecipheriv } from 'crypto'
 
 export const decrypt = (secretKey: string, text: string): string => {
   const textParts = decodeURIComponent(text).split(':') as string[]
-  if (!textParts.length) return ''
-
+  if (textParts.length < 2) return ''
   const iv = Buffer.from(textParts.shift() as string, 'hex')
   const encryptedText = Buffer.from(textParts.join(':'), 'hex')
-  const decipher = createDecipheriv(
-    'aes-256-cbc',
-    Buffer.from(secretKey, 'hex').subarray(0, 32),
-    iv
-  )
+  const keyBuffer = Buffer.from(secretKey, 'hex')
+  if (keyBuffer.length !== 32) {
+    throw new Error('A chave deve ter 32 bytes')
+  }
+  const decipher = createDecipheriv('aes-256-cbc', keyBuffer, iv)
   let decrypted = decipher.update(encryptedText)
   decrypted = Buffer.concat([decrypted, decipher.final()])
   return decrypted.toString()
