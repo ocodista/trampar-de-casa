60b611551871d5db957e1032c7ce1edaec120bab
Luís Fernando de Paulo Sousa Oliveira <luis.oliveirabr1@gmail.com>
2024-09-16 13:06:15 -0300

    fix: router on jobCard (#309)

--- apps/web/app/components/ui/JobCard.tsx
+++ apps/web/app/components/ui/JobCard.tsx
@@ -6,7 +6,7 @@ const jobCard = ({ job, skillsFromProps }) => {
 
   return (
     <a
-      href={`https://router.trampardecasa.com/api/role-access?roleId=${job.id}`}
+      href={`https://router.trampardecasa.com.br/api/role-access?roleId=${job.id}`}
       target="_blank"
       className="pointer shadow-brand-shadow border-box w-full rounded-lg border-[1px] bg-[#FCFCFD] p-[30px] hover:border-[1px] hover:border-[#4f46e5] "
     >
