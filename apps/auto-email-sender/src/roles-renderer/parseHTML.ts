a178098ad072768ba1f84c90408e60181bc27555
Caio Borghi <caiohoborghi@gmail.com>
2024-09-16 17:49:33 -0300

    add router url to sender

--- apps/auto-email-sender/src/roles-renderer/parseHTML.ts
+++ apps/auto-email-sender/src/roles-renderer/parseHTML.ts
@@ -4,13 +4,16 @@ import { RoleHTML } from 'shared/ui/email/RoleHTML'
 type RolesSkillsView = Database['public']['Views']['RolesSkillsView']['Row']
 export const htmlStartingDoctype = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">`
 
+const routerURL = (roleId: string) =>
+  `https://router.trampardecasa.com.br/api/role-access?roleId=${roleId}`
+
 export const parseHTML = (role: RolesSkillsView): string => {
   return RoleHTML({
     company: role.company,
     language: role.language,
     headerInfo: role.description,
     location: role.country,
-    url: role.url,
+    url: routerURL(role.id as string),
     title: role.title,
     skills: role.skillNames,
     salary: role.salary,
