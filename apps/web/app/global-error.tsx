27f39df3cc190168474bff56ddd421863cfd38f2
Luís Fernando de Paulo Sousa Oliveira <luis.oliveirabr1@gmail.com>
2024-09-24 15:52:55 -0300

    feat: update global error screen (#313)
    ![image](https://github.com/user-attachments/assets/6771413f-ef12-499c-a7a9-40308378f0f1)
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Caio Borghi <caiohoborghi@gmail.com>

--- apps/web/app/global-error.tsx
+++ apps/web/app/global-error.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import Image from 'next/image'
+import errorImage from 'public/images/errorImage.png'
+
 export default function GlobalError({
   reset,
 }: {
@@ -8,12 +11,30 @@ export default function GlobalError({
 }) {
   return (
     <html>
-      <body>
-        <h2>Algo deu errado!</h2>
-        <button onClick={() => reset()}>Tente novamente</button>
-        <a className="pl-2" href="/">
-          Volte para a página inicial
-        </a>
+      <body className="flex min-h-screen items-center justify-center bg-amber-50 p-4">
+        <div className="max-w-2xl rounded-lg bg-white p-8 text-center shadow-xl">
+          <Image
+            src={errorImage}
+            alt="Escritório bagunçado com sinais de 'Sem café'"
+            width={600}
+            height={600}
+            className="mb-6 rounded-lg"
+          />
+          <h2 className="text-brown-700 mb-4 text-3xl font-bold">
+            Oops! Acabou o café!
+          </h2>
+          <p className="mb-6 text-lg text-gray-600">
+            Parece que estamos enfrentando um erro.
+          </p>
+          <div className="flex justify-center space-x-4">
+            <a
+              href="/"
+              className="rounded bg-amber-500 px-4 py-2 text-center font-bold text-white transition duration-300 hover:bg-amber-600"
+            >
+              Voltar para a Página Inicial
+            </a>
+          </div>
+        </div>
       </body>
     </html>
   )
