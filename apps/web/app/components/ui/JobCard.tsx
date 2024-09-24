7bd387cbf5c17e7c1d85ba30b85d94047c1bbb2a
Caio Borghi <caiohoborghi@gmail.com>
2024-09-24 14:12:46 -0300

    add redis and fix JobCard

--- apps/web/app/components/ui/JobCard.tsx
+++ apps/web/app/components/ui/JobCard.tsx
@@ -18,7 +18,7 @@ const JobCard = ({ job, skillsFromProps }) => {
 
   return (
     <a
-      href={trackedRoleURL(job.url)}
+      href={trackedRoleURL(job.id)}
       target="_blank"
       className="shadow-brand-shadow border-box w-full cursor-pointer rounded-lg border-[1px] bg-[#FCFCFD] p-[30px] hover:border-[1px] hover:border-[#4f46e5] "
       rel="noreferrer"
