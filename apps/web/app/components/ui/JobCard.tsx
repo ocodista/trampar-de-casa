e9f5691cf5f93915769c934d5266ef736d1789b5
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-10-04 18:08:50 -0300

    fix: ui of JobCard

--- apps/web/app/components/ui/JobCard.tsx
+++ apps/web/app/components/ui/JobCard.tsx
@@ -43,41 +43,35 @@ const JobCard = ({ job, skillsFromProps }) => {
     <a
       href="#"
       onClick={handleClick}
-      className="shadow-brand-shadow border-box w-full cursor-pointer rounded-lg border-[1px] bg-[#FCFCFD] p-[30px] hover:border-[1px] hover:border-[#4f46e5] "
+      className="shadow-brand-shadow border-box mb-4 block w-full cursor-pointer rounded-lg border border-gray-200 bg-white p-8 hover:border-blue-500"
       rel="noreferrer"
     >
-      <div className="mb-[10px] flex flex-wrap items-center justify-between">
-        <h2 className="text-[11px] sm:text-[12px] md:text-[12px] lg:text-[12px]">
-          {job.company}
-        </h2>
+      <div className="flex justify-between">
+        <p className="mb-2 text-sm text-gray-600">{job.company}</p>
         <span className="text-[13px] sm:text-[16px] md:text-[16px] lg:text-[16px]">
           {salaryText}
         </span>
       </div>
-      <h1 className="mb-[10px] text-[15px] font-bold sm:text-[17px] md:text-[17px] lg:text-[17px]">
-        {job.title}
-      </h1>
-      <div className=" mb-[25px] flex flex-wrap gap-[15px] sm:mb-[35px] md:mb-[35px] lg:mb-[35px]">
-        {job.skillsId.map((skillId) => (
+      <h2 className="text-lg font-semibold">{job.title}</h2>
+      <div className="my-4">
+        {job.skillsId.slice(0, 1).map((skillId) => (
           <span
             key={skillId}
             className={`${
               normalize(skillId) === null && 'hidden'
-            } mt-[5px] rounded-[20px] border-[1px] border-black bg-[#F4F4F5] px-[15px] py-[2px] text-[14px] sm:text-[16px] md:text-[16px] lg:text-[16px]`}
+            } mt-[5px] rounded-[20px] border-[1px] border-black bg-[#F4F4F5] px-[15px] py-[4px] text-[14px] sm:text-[16px] md:text-[16px] lg:text-[16px]`}
           >
             {normalize(skillId)}
           </span>
         ))}
       </div>
-      <div className="flex gap-[25px] text-[13px] sm:text-[15px] md:text-[15px] lg:text-[15px]">
-        <div className="flex items-center">
-          <span>📍 {job.country ? job.country : 'Internacional'}</span>
-        </div>
-        <div className="flex items-center">
-          <span>
-            💬 {job.language === 'Portuguese' ? 'Português' : job.language}
-          </span>
-        </div>
+      <div className="flex items-center text-sm text-gray-600">
+        <span className="mr-4">
+          📍 {job.country ? job.country : 'Internacional'}
+        </span>
+        <span>
+          💬 {job.language === 'Portuguese' ? 'Português' : job.language}
+        </span>
       </div>
     </a>
   )
