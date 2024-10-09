822c3bfb2aacc2fb7f6b0a5214de6efd78ca4f8e
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-10-09 15:23:39 -0300

    feat: added ordering on initial fetch if have cache

--- apps/web/app/(roles)/vagas/page.tsx
+++ apps/web/app/(roles)/vagas/page.tsx
@@ -1,27 +1,49 @@
 'use server'
 
-import { getSupabaseClient } from 'db'
+import { Database, getSupabaseClient } from 'db'
 import { RolesPage } from './RolesPage'
 import { fetchJobs } from './action'
 import getRedisClient from 'app/utils/getRedisClient'
 
 const ONE_DAY_IN_MINUTES = 86_400
 
-async function getJobs() {
+type Job = Database['public']['Tables']['Roles']['Row']
+
+async function getJobs(): Promise<Job[]> {
   try {
     const client = await getRedisClient()
     const jobsFromCache = await client.get('web_jobs')
-    // if (jobsFromCache) {
-    //   await client.quit()
-    //   return JSON.parse(jobsFromCache)
-    // }
 
-    const { data: jobs } = await fetchJobs([])
+    let jobs: Job[]
+    if (jobsFromCache) {
+      jobs = JSON.parse(jobsFromCache) as Job[]
+      console.log('cache')
+      jobs = jobs.sort((a, b) => {
+        const parseSalary = (salary: string | null) => {
+          if (!salary) return 0
+          const cleanedSalary = salary.replace(/[^0-9.-]+/g, '')
+          return parseFloat(cleanedSalary) || 0
+        }
 
-    await client.set('web_jobs', JSON.stringify(jobs), {
-      EX: ONE_DAY_IN_MINUTES,
-    })
+        const salaryA = parseSalary(a.salary)
+        const salaryB = parseSalary(b.salary)
+
+        if (salaryA === salaryB) return 0
+        if (salaryA === 0) return 1
+        if (salaryB === 0) return -1
+        return salaryB - salaryA
+      })
+    } else {
+      const { data: fetchedJobs } = await fetchJobs([])
+      jobs = fetchedJobs as Job[]
+
+      await client.set('web_jobs', JSON.stringify(jobs), {
+        EX: ONE_DAY_IN_MINUTES,
+      })
+    }
+
     await client.quit()
+
     return jobs
   } catch (error) {
     console.error('Failed to fetch jobs:', error)
