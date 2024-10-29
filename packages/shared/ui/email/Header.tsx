376b5eb895b715d9bedb030ea15b04c174bc0525
Caio Borghi <caiohoborghi@gmail.com>
2024-10-29 14:36:51 -0300

    update email

--- packages/shared/ui/email/Header.tsx
+++ packages/shared/ui/email/Header.tsx
@@ -70,30 +70,7 @@ export function Header({
       <Hr style={hr} />
       <Text className={paragraph}>Salve, beleza?</Text>
       <Text className={paragraph}>
-        O trabalho remoto é o <strong>novo normal</strong>, e o Spotify tá
-        mostrando como se faz!
-      </Text>
-      <Text className={paragraph}>
-        Eles acabaram de reafirmar o compromisso com o trabalho remoto, dizendo
-        que seus funcionários <Link href="https://www.techradar.com/pro/spotify-says-it-will-allow-staff-to-work-from-home-says-employees-arent-children">não são crianças</Link> 😁 
-      </Text>
-      <Text className={paragraph}>
-      Nada melhor do que uma empresa que enxerga seus funcionários como adultos responsáveis 🙏
-      </Text>
-      <Text className={paragraph}>
-        Mas e aí, já comprou seu ingresso do FrontInSampa? Já é sábado agora, agiliza!
-      </Text>
-      <Text className={paragraph}>
-        15% off com o cupom{' '}
-        <Link href="https://fastix.com.br/events/front-in-sampa?coupon=TRAMPARDCASA">
-          TRAMPARDCASA
-        </Link>
-      </Text>
-      <Text className={paragraph}>
-        <i>
-          Sem mais delongas, aqui estão suas vagas 100% remotas ✌️ 😁
-          remoto!
-        </i>
+        <i>Aproveite suas vagas!</i>
       </Text>
     </Tailwind>
   )
