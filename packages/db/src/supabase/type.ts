6d7906405b8ba9ebbf00ad8b7fca5cb7e6275de3
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-10-29 11:37:51 -0300

    feat: details column on db, update types

--- packages/db/src/supabase/type.ts
+++ packages/db/src/supabase/type.ts
@@ -12,6 +12,7 @@ export type Database = {
       RoleApplications: {
         Row: {
           createdAt: string
+          details: Json | null
           id: string
           meetsRequirements: boolean
           roleId: string
@@ -21,6 +22,7 @@ export type Database = {
         }
         Insert: {
           createdAt?: string
+          details?: Json | null
           id?: string
           meetsRequirements: boolean
           roleId: string
@@ -30,6 +32,7 @@ export type Database = {
         }
         Update: {
           createdAt?: string
+          details?: Json | null
           id?: string
           meetsRequirements?: boolean
           roleId?: string
---
+++ packages/db/supabase/migrations/20241025211731_add_details_column.sql
@@ -0,0 +1,3 @@
+alter table "public"."RoleApplications" add column "details" jsonb;
+
+
