7f871bdc2d7431f2dc919eac0637575407cd4d8c
Caio Borghi <caiohoborghi@gmail.com>
2025-04-03 17:08:19 -0300

    chore: remove custom web app CI workflow and update Redis configuration
    - Deleted the custom CI workflow for building and pushing the web app, streamlining the CI process.
    - Updated the .env.example file to rename Redis configuration from REDIS_HOST and REDIS_PORT to REDIS_URL for better clarity.
    - Modified getRedisClient to use the new REDIS_URL environment variable, improving code consistency.
    
    These changes simplify the CI setup and enhance the configuration management for Redis.

--- apps/web/app/utils/getRedisClient.ts
+++ apps/web/app/utils/getRedisClient.ts
@@ -2,10 +2,7 @@ import { createClient as createClientRedis } from 'redis'
 
 export async function getRedisClient() {
   const client = createClientRedis({
-    socket: {
-      host: process.env['REDIS_HOST'],
-      port: parseInt(process.env['REDIS_PORT'] || '6379'),
-    },
+    url: process.env['REDIS_URL'],
   })
   await client.connect()
   return client
