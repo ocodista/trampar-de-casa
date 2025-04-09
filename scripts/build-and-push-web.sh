7b52f63b7a00ff9cd5c3c81e9c7d0f9dacf1b1dd
Caio Borghi <caiohoborghi@gmail.com>
2025-04-08 22:30:58 -0300

    chore: refactor build-and-push-web script to use Docker secrets
    - Removed individual environment variable exports in favor of a temporary secrets file for improved security and simplicity.
    - Updated the Docker build command to utilize the new secrets management approach, enhancing the maintainability of the build process.
    - Added cleanup step to remove the temporary secrets file after the build.
    
    These changes streamline the build process and enhance security by managing sensitive information more effectively.

--- scripts/build-and-push-web.sh
+++ scripts/build-and-push-web.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 set -e
 
 if [ ! -f .secrets ]; then
@@ -13,33 +11,20 @@ PLATFORM=${1:-amd64}
 REGISTRY=${DOCKER_REGISTRY_HOST}
 REPO='trampar-de-casa'
 
-# Export all environment variables to make them available to the build
-export EMAIL_KEY
-export EMAIL_PASS
-export CRYPT_SECRET
-export RESEND_KEY
-export RESEND_WEBHOOK_SECRET
-export OWNER_EMAIL
-export NEXT_PUBLIC_MIXPANEL_KEY
-export CRON_SECRET
-export POSTGRES_URL
-export REDIS_URL
+# Create temporary secrets file
+TEMP_SECRETS=$(mktemp)
+cat .secrets > "$TEMP_SECRETS"
 
 cd ../
 
 VERSION=$(cd apps/web && cat package.json | jq -r '.version')
 
+# Build and push with secrets
 docker buildx build . --platform linux/$PLATFORM \
 	-t "$REGISTRY/$REPO:latest" \
 	-t "$REGISTRY/$REPO:$VERSION" \
 	--push \
-	--build-arg EMAIL_KEY="$EMAIL_KEY" \
-	--build-arg EMAIL_PASS="$EMAIL_PASS" \
-	--build-arg CRYPT_SECRET="$CRYPT_SECRET" \
-	--build-arg RESEND_KEY="$RESEND_KEY" \
-	--build-arg RESEND_WEBHOOK_SECRET="$RESEND_WEBHOOK_SECRET" \
-	--build-arg OWNER_EMAIL="$OWNER_EMAIL" \
-	--build-arg NEXT_PUBLIC_MIXPANEL_KEY="$NEXT_PUBLIC_MIXPANEL_KEY" \
-	--build-arg CRON_SECRET="$CRON_SECRET" \
-	--build-arg POSTGRES_URL="$POSTGRES_URL" \
-	--build-arg REDIS_URL="$REDIS_URL" 
+	--secret id=secrets,src="$TEMP_SECRETS"
+
+# Clean up
+rm "$TEMP_SECRETS" 
